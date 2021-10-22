d44c71f213a2754f06a7698d410a0a43f2cdf5a9
leobar37 <usatloqueando@gmail.com>
2021-10-22 11:27:23 -0500

    chore : I ma leave this proyect :(

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # Kacera landing
 
-[proto](https://xd.adobe.com/view/b400ac07-de94-4d38-84d7-01b9ca4e1903-e153/)
+[Prototipo](https://xd.adobe.com/view/b400ac07-de94-4d38-84d7-01b9ca4e1903-e153/)
 
+## Tecnologias
 
+- [React](https://es.reactjs.org/) : Usado como librería para construir las interfaces
+- [chakra-ui](https://chakra-ui.com/) : Libreria de Ui basado en CSS-IN-JS
+- [React Query](https://react-query.tanstack.com/) : Utilizado para gestionar el estado Cliente-servidor
+- [zuztand](https://github.com/pmndrs/zustand) : Librería de manejo de estado alternativa a redux
+- [nextjs](https://nextjs.org/): Framework basado en react para hacer SSR y CSR
 
+## Estructura del proyecto
+
+Este proyecto a sido organizado orientado a módulos,cada modulo es una funcionalidad, cualquier duda,dejar una issue
+en el repo no dudare en responder.
+
+# Api
+
+Esta es la documentación de la [api](https://docs.google.com/document/d/1zBGx0ONIxO-Ww7MhXjlAVRE01EpoCXRMXRmloM5IjnA/edit)
+
+**Good luck**
--- src/modules/onboarding/ubication/ModalUbication.tsx
+++ src/modules/onboarding/ubication/ModalUbication.tsx
@@ -1,31 +1,84 @@
-import React from "react";
+import React from 'react';
 import {
   Modal,
   ModalContent,
-  ModalBody,
-  ModalHeader,
   CloseButton,
+  Heading,
   useDisclosure,
   Box,
-} from "@chakra-ui/react";
-import GoogleMapReact from "google-map-react";
+  Text,
+  HStack,
+  VStack,
+  Center,
+  Input,
+  InputGroup,
+  chakra,
+  InputLeftElement,
+  useToken,
+  ModalCloseButton,
+} from '@chakra-ui/react';
+import GoogleMapReact from 'google-map-react';
+import { FaMapMarkerAlt as _MapIcon } from 'react-icons/fa';
+import { useState, useEffect } from 'react';
+const MapIcon = chakra(_MapIcon);
+
+const useLocationCoord = () => {
+  const [position, setPosition] = useState({ latitude: 0, logitud: 0 });
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      setPosition({
+        latitude: pos.coords.latitude,
+        logitud: pos.coords.longitude,
+      });
+    });
+  }, [position]);
+  return position;
+};
+
 export function ModalUbication() {
   const { isOpen, onClose, onOpen } = useDisclosure({
     isOpen: true,
   });
+  const position = useLocationCoord();
+  const primaryColor = useToken('colors', ['primary']);
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <CloseButton />
-
       <ModalContent>
-        <Box width="450px" height="350px">
-          <GoogleMapReact
-            defaultCenter={{ lat: 59.95, lng: 30.33 }}
-            zoom={12}
-            bootstrapURLKeys={{
-              key: "AIzaSyBrOgmvq8HrfwKOsgjqGB8v6Ma04awmiEE",
-            }}
-          />
+        <Box
+          position="relative"
+          borderRadius="20px"
+          padding="40px 50px"
+          width="550px"
+          bg="white"
+          border={`2px solid ${primaryColor}`}
+        >
+          <ModalCloseButton pos="absolute" right="20px" />
+          <VStack my={2}>
+            <Heading fontSize="3xl" textAlign="center">
+              Para mostrarte las tiendas cerca
+            </Heading>
+            <Text color="gray.500" fontSize="small">
+              Necesito <Text as="b">saber tu ubicación de entrega</Text>
+            </Text>
+          </VStack>
+          <Box my={5}>
+            <InputGroup>
+              <InputLeftElement>
+                <MapIcon color="gray.400" />
+              </InputLeftElement>
+              <Input placeholder="22 de julio 245,Lince Lima" />
+            </InputGroup>
+          </Box>
+          <Center width="100%" height="350px" overflow="hidden">
+            <GoogleMapReact
+              defaultCenter={{ lat: position.latitude, lng: position.logitud }}
+              zoom={12}
+              bootstrapURLKeys={{
+                key: 'AIzaSyAMSHuJEc8DPD0f1JAA9VM_c2-MGwnO8zc',
+              }}
+            />
+          </Center>
         </Box>
       </ModalContent>
     </Modal>
